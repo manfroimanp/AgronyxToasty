623c12997eb2762d449d967f356d9335659502d3
manfroi_diogo <manfroi@manptecnologia.com.br>
2022-03-09 16:30:38 -0300

    Initial commit

---
+++ app/src/main/java/com/manp/toasty/MainActivity.java
@@ -0,0 +1,16 @@
+package com.manp.toasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.simpleToast(this,"AGRONYX");
+    }
+}
